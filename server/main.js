84dd0a7816abf47ec57e135a64eb0752cd40e32c
Wolfram Esser <github@derwok.de>
2017-01-14 09:32:25 +0100

    Added Meteor-Files to provoke server side exception on login

--- server/main.js
+++ server/main.js
@@ -2,8 +2,12 @@ import { Meteor } from 'meteor/meteor';
 
 import { Accounts } from 'meteor/accounts-base'
 
+import { FilesCollection } from 'meteor/ostrio:files';
+
 Meteor.startup(() => {
     Accounts.onLogin(function () {
         console.log("Login of user: "+Accounts.userId());
     })
+
+
 });
